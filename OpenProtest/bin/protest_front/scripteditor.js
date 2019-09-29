c976cc5735147aadac940c6ff5e0eb20a8ce60fa
venizelou andreas <bl4ckdn4@msn.com>
2019-09-29 18:54:02 +0300

    Scripts front-end, parameters

--- OpenProtest/bin/protest_front/scripteditor.js
+++ OpenProtest/bin/protest_front/scripteditor.js
@@ -3,35 +3,35 @@
 //p: optional input
 
 //c: column
-//n: numeric property, min, max
-//t: text property
+//h: checkbox (preset)
+//n: numeric (preset, min, max)
+//t: text (preset)
 //m: multiline
-//s: separator
 
 const TOOLS_ARRAY = [
-    {name:"Protest users",            color:"rgb(232,118,0)", p:[["o","Output"]]},
-    {name:"Protest equipment",        color:"rgb(232,118,0)", p:[["o","Output"]]},
-    {name:"Acrive dir. users",        color:"rgb(232,118,0)", p:[["o","Output"]]},
-    {name:"Acrive dir. workstations", color:"rgb(232,118,0)", p:[["o","Output"]]},
+    {name:"Protest users",       color:"rgb(232,118,0)", p:[["o","Output"]]},
+    {name:"Protest equipment",   color:"rgb(232,118,0)", p:[["o","Output"]]},
+    {name:"Domain users",        color:"rgb(232,118,0)", p:[["o","Output"]]},
+    {name:"Domain workstations", color:"rgb(232,118,0)", p:[["o","Output"]]},
 
-    {name:"SNMP query",   color:"rgb(32,32,32)", p:[["p","Host"], ["m","Query"], ["o","Output"]]},
-    {name:"WMI query",    color:"rgb(32,32,32)", p:[["p","Host"], ["m","Query"], ["o","Output"]]},
-    {name:"PS Exec",      color:"rgb(32,32,32)", p:[["p","Host"], ["m","Command"], ["o","Output"]]},
-    {name:"Secure Shell", color:"rgb(32,32,32)", p:[["p","Host"], ["m","Command"], ["o","Output"]]},
+    {name:"SNMP query",   color:"rgb(32,32,32)", p:[["p","Host",""], ["m","Query",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"WMI query",    color:"rgb(32,32,32)", p:[["p","Host",""], ["m","Query",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"PS Exec",      color:"rgb(32,32,32)", p:[["p","Host",""], ["m","Command",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"Secure Shell", color:"rgb(32,32,32)", p:[["p","Host",""], ["m","Command",""], ["h","Async","True"], ["o","Output"]]},
 
-    {name:"ARP",         color:"rgb(232,0,0)", p:[["p","Host"], ["o","Output"]]},
-    {name:"DNS",         color:"rgb(232,0,0)", p:[["p","Host"], ["o","Output"]]},
-    {name:"Ping",        color:"rgb(232,0,0)", p:[["p","Host"], ["n","Time out"], ["o","Output"]]},
-    {name:"Trace route", color:"rgb(232,0,0)", p:[["p","Host"], ["o","Output"]]},
-    {name:"Port scan",   color:"rgb(232,0,0)", p:[["p","Host"], ["n","From"], ["n","To"], ["o","Output"]]},
-    {name:"Locate IP",   color:"rgb(232,0,0)", p:[["p","Host"], ["o","Output"]]},
-    {name:"MAC loopup",  color:"rgb(232,0,0)", p:[["p","Host"], ["o","Output"]]},
+    {name:"ARP",         color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"DNS",         color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"Ping",        color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["n","Time out",1000,200,5000], ["o","Output"]]},
+    {name:"Trace route", color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"Port scan",   color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["n","From",1,1,65535], ["n","To",49152,1,65535], ["o","Output"]]},
+    {name:"Locate IP",   color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["o","Output"]]},
+    {name:"MAC loopup",  color:"rgb(232,0,0)", p:[["p","Host",""], ["h","Async","True"], ["o","Output"]]},
 
     {name:"Unique",   color:"rgb(0,118,232)", p:[["i","Input"], ["c","Column"], ["o","Output"]]},
     {name:"Sort",     color:"rgb(0,118,232)", p:[["i","Input"], ["c","Column"], ["o","Output"]]},
     {name:"Column",   color:"rgb(0,118,232)", p:[["i","Input"], ["c","Column"], ["o","Output"]]},
-    {name:"Equals",   color:"rgb(0,118,232)", p:[["i","Input"], ["t","Value"], ["c","Column"], ["o","Output"]]},
-    {name:"Contains", color:"rgb(0,118,232)", p:[["i","Input"], ["t","Value"], ["c","Column"], ["o","Output"]]},
+    {name:"Equals",   color:"rgb(0,118,232)", p:[["i","Input"], ["t","Value",""], ["c","Column"], ["o","Output"]]},
+    {name:"Contains", color:"rgb(0,118,232)", p:[["i","Input"], ["t","Value",""], ["c","Column"], ["o","Output"]]},
 
     {name:"Absolute value", color:"rgb(111,212,43)", p:[["i","Input"], ["c","Column"], ["o","Output"]]},
     {name:"Round",          color:"rgb(111,212,43)", p:[["i","Input"], ["c","Column"], ["o","Output"]]},
@@ -101,13 +101,17 @@ class ScriptEditor extends Window {
         this.ghost.className = "script-ghost-node";
         this.win.appendChild(this.ghost);
 
-        this.properties = document.createElement("div");
-        this.properties.className = "script-properties";
-        this.content.appendChild(this.properties);
+        this.parameters = document.createElement("div");
+        this.parameters.className = "script-parameters";
+        this.content.appendChild(this.parameters);
 
-        this.propertiesName = document.createElement("div");
-        this.propertiesName.className = "script-selected-name";
-        this.properties.appendChild(this.propertiesName);
+        this.selectedName = document.createElement("div");
+        this.selectedName.className = "script-selected-name";
+        this.parameters.appendChild(this.selectedName);
+
+        this.parametersList = document.createElement("div");
+        this.parametersList.className = "script-parameters-list";
+        this.parameters.appendChild(this.parametersList);
 
         this.ghost.onmouseup = event => this.Ghost_onmouseup(event);
 
@@ -136,17 +140,101 @@ class ScriptEditor extends Window {
         }
     }
 
-    ShowProperties(node) {
+    ShowParameters(node) {
+        if (this.selectedNode === node) return;
+
         if (this.selectedNode !== null) {
             this.selectedNode.container.setAttribute("stroke", "rgb(0,0,0)");
             this.selectedNode.container.setAttribute("stroke-width", ".5");
+            for (let i = 0; i < this.selectedNode.links.length; i++) {
+                this.selectedNode.links[i][1].setAttribute("stroke", "rgb(0,0,0)");
+                this.selectedNode.links[i][1].setAttribute("stroke-width", ".5");
+            }
         }
 
         node.container.setAttribute("stroke", "var(--select-color)");
         node.container.setAttribute("stroke-width", "3");
+        for (let i = 0; i < node.links.length; i++) {
+            node.links[i][1].setAttribute("stroke", "var(--select-color)");
+            node.links[i][1].setAttribute("stroke-width", "2");
+        }
 
         this.selectedNode = node;
-        this.propertiesName.innerHTML = node.title;
+        this.selectedName.innerHTML = node.name;
+
+        //Show parameters
+        this.parametersList.innerHTML = "";
+        for (let i = 0; i < node.parameters.length; i++) {
+            if (node.parameters[i][0]=="o") continue; //skip ouputs
+
+            let newPara = document.createElement("div");
+            this.parametersList.appendChild(newPara);
+
+            let label = document.createElement("div");
+            label.innerHTML = node.parameters[i][1] + ":";
+            newPara.appendChild(label);
+
+            let value;
+            if (node.parameters[i][0] == "t" || node.parameters[i][0] == "p") { //text
+                value = document.createElement("input");
+                value.type = "text";
+                value.value = node.values[i]===null ? "" : node.values[i];
+
+            } else if (node.parameters[i][0] == "n") { //number
+                value = document.createElement("input");
+                value.type = "number";
+                value.min = node.parameters[i][3];
+                value.max = node.parameters[i][4];
+                value.value = node.values[i]===null ? value.min : node.values[i];
+
+            } else if (node.parameters[i][0] == "h") { //checkbox
+                value = document.createElement("select");
+
+                let optTrue = document.createElement("option");
+                optTrue.innerHTML = "True";
+                optTrue.value = "True";
+                value.appendChild(optTrue);
+
+                let optFalse = document.createElement("option");
+                optFalse.innerHTML = "False";
+                optFalse.value = "False";
+                value.appendChild(optFalse);
+
+                value.value = node.values[i] === null ? value.min : node.values[i];
+
+            } else if (node.parameters[i][0] == "c") { //column
+                value = document.createElement("select");
+
+            } else if (node.parameters[i][0] == "m") { //multiline
+                value = document.createElement("input");
+                value.type = "button";
+                value.value = "Edit";
+                value.onclick = () => {
+                    //TODO:
+                };
+
+            } else { //i
+                value = document.createElement("div");
+                value.innerHTML = "null";
+            }
+            value.setAttribute("i", i);
+            newPara.appendChild(value);
+
+            value.onchange = event => {
+                let index = parseInt(event.target.getAttribute("i"));
+                node.values[index] = value.value;
+            };
+     
+            if (node.parameters[i][0] != "m" && node.parameters[i][0] != "h") {
+                let button = document.createElement("div");
+                newPara.appendChild(button);
+                button.onclick = () => {
+                    if (value.tagName === "div") return;
+                    value.value = "";
+                };
+            }
+
+        }
     }
 
     Ghost_onmouseup(event) {
@@ -164,7 +252,7 @@ class ScriptEditor extends Window {
 
         this.nodes.push(newNode);
 
-        this.ShowProperties(newNode);
+        this.ShowParameters(newNode);
     }
 
     Node_onmousedown(event, node) {
@@ -179,7 +267,7 @@ class ScriptEditor extends Window {
         this.x0 = event.clientX;
         this.y0 = event.clientY;
 
-        this.ShowProperties(node);
+        this.ShowParameters(node);
     }
 
     Node_onmousemove(event) {
@@ -201,10 +289,10 @@ class ScriptEditor extends Window {
 }
 
 class ScriptListTool {
-    constructor(name, color, paramiters, parent) {
+    constructor(name, color, parameters, parent) {
         this.name = name;
         this.color = color;
-        this.p = paramiters;
+        this.p = parameters;
         this.parent = parent;
 
         this.element = document.createElement("div");
@@ -267,13 +355,15 @@ class ScriptListTool {
 
 class ScriptNode {
     constructor(tool) {
-        this.title = tool.name;
         this.x = 0;
         this.y = 0;
+        this.name = tool.name;
+        this.parameters = [];
+        this.values     = [];
+        this.links      = [];
+        this.columns    = [];
 
         this.g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-        //this.g.setAttribute("x", 0);
-        //this.g.setAttribute("y", 0);
 
         this.container = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         this.container.setAttribute("width", 200);
@@ -301,7 +391,6 @@ class ScriptNode {
         this.titleText.setAttribute("font-weight", "600");
         this.titleText.setAttribute("dominant-baseline", "middle");
         this.titleText.setAttribute("text-anchor", "middle");
-        //this.titleText.setAttribute("text-decoration", "underline");
         this.titleText.setAttribute("x", 100);
         this.titleText.setAttribute("y", 14);
         this.titleText.setAttribute("fill", "rgb(224,224,224)");
@@ -310,59 +399,38 @@ class ScriptNode {
         let top = 38;
 
         for (let i = 0; i < tool.p.length; i++) {
+            this.parameters.push(tool.p[i]); //copy
+            this.values.push(tool.p[i].length > 2 ? tool.p[i][2] : null);
 
-            console.log(tool.p);
-
-            if (tool.p[i][0]=="o") { //output
-                let outp = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                outp.id = "dot";
-                outp.setAttribute("r", 6);
-                outp.setAttribute("cx", 200);
-                outp.setAttribute("cy", top);
-                outp.setAttribute("fill", "rgb(96,96,96)");
-                outp.setAttribute("stroke", "rgb(0,0,0)");
-                outp.setAttribute("stroke-width", ".5");
-                this.g.appendChild(outp);
-
-                let label = document.createElementNS("http://www.w3.org/2000/svg", "text");
-                label.innerHTML = tool.p[i][1];
-                label.setAttribute("dominant-baseline", "middle");
-                label.setAttribute("text-anchor", "end");
-                label.setAttribute("x", 188);
-                label.setAttribute("y", top);
-                label.setAttribute("fill", "rgb(224,224,224)");
-                this.g.appendChild(label);
-
-                outp.onmousedown = event => { event.stopPropagation(); };
-
-                top += 24;
-
-            } else if (tool.p[i][0] == "i" || tool.p[i][0] == "p") { //input
-                let inp = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                inp.id = "dot";
-                inp.setAttribute("r", 6);
-                inp.setAttribute("cx", 0);
-                inp.setAttribute("cy", top);
-                inp.setAttribute("fill", "rgb(96,96,96)");
-                inp.setAttribute("stroke", "rgb(0,0,0)");
-                inp.setAttribute("stroke-width", ".5");
-                this.g.appendChild(inp);
+            if (tool.p[i][0] == "i" || tool.p[i][0] == "p" || tool.p[i][0] == "o") { //input or output
+                let link = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+                link.id = "dot";
+                link.setAttribute("r", 6);
+                link.setAttribute("cx", tool.p[i][0]=="o" ? 200 : 0);
+                link.setAttribute("cy", top-1);
+                link.setAttribute("fill", "rgb(96,96,96)");
+                link.setAttribute("stroke", "rgb(0,0,0)");
+                link.setAttribute("stroke-width", ".5");
+                this.g.appendChild(link);
 
                 let label = document.createElementNS("http://www.w3.org/2000/svg", "text");
                 label.innerHTML = tool.p[i][1];
                 label.setAttribute("dominant-baseline", "middle");
-                label.setAttribute("text-anchor", "start");
-                label.setAttribute("x", 12);
+                label.setAttribute("text-anchor", tool.p[i][0]=="o" ? "end" : "start");
+                label.setAttribute("x", tool.p[i][0]=="o" ? 188 : 12);
                 label.setAttribute("y", top);
                 label.setAttribute("fill", "rgb(224,224,224)");
                 this.g.appendChild(label);
 
-                inp.onmousedown = event => { event.stopPropagation(); };
+                this.links.push([tool.p[i][0], link, label]);
+
+                link.onmousedown = event => { event.stopPropagation(); };
 
                 top += 24;
             }
         }
 
+        this.container.setAttribute("height", Math.max(top-10, 75));
     }
 
     Attach(container) {
