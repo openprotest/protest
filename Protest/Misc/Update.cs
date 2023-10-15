26084fabfd745d3cceba51fdfc606440cbc562b4
venizelou andreas <bl4ckdn4@msn.com>
2023-10-15 23:48:18 +0300

    Update tor knowledge base method

--- Protest/Misc/Update.cs
+++ Protest/Misc/Update.cs
@@ -428,10 +428,10 @@ internal static class Update {
         }
 
         list.Sort((byte[] a, byte[] b)=> {
-            if (a[0] != b[0]) return Comparer<byte>.Default.Compare(a[0], b[0]);
-            if (a[1] != b[1]) return Comparer<byte>.Default.Compare(a[1], b[1]);
-            if (a[2] != b[2]) return Comparer<byte>.Default.Compare(a[2], b[2]);
-            if (a[3] != b[3]) return Comparer<byte>.Default.Compare(a[3], b[3]);
+            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[1] != b[1]) return a[1] - b[1];
+            if (a[2] != b[2]) return a[2] - b[2];
+            if (a[3] != b[3]) return a[3] - b[3];
             return 0;
         });
 
