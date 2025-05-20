4fb771f8f7052b459f90390af95b942225279ea8
Andreas Venizelou <Bl4ckDn4@msn.com>
2025-05-20 15:49:38 +0300

    SNMP, binary string

--- Protest/Protocols/Snmp.Polling.cs
+++ Protest/Protocols/Snmp.Polling.cs
@@ -337,38 +337,27 @@ internal static partial class Polling {
                 builder.Append("\"--\"");
             }
             else if (result[i].Data.TypeCode == SnmpType.OctetString) {
-                byte[] bytes = result[i].Data.ToBytes();
-                
-                int from = 0, to = bytes.Length;
-                if (bytes.Length > 2) {
-                    from = 2;
-                    to = Math.Min(bytes[1], bytes.Length);
-                }
+                string s = result[i].Data.ToString();
+                bool isBinary = false;
 
-                for (int j = to; j > from; j--) {
-                    if (bytes[j] > 0) break;
-                    to = j;
-                }
-
-                bool isPureString = true;
-                for (int j = from; j < to; j++) {
-                    if (bytes[j] < 32 || bytes[j] > 126) {
-                        isPureString = false;
+                for (int j = 0; j < s.Length; j++) {
+                    if (s[j] < 32 || s[j] > 126) {
+                        isBinary = true;
                         break;
                     }
                 }
 
-                if (isPureString) {
-                    string s = Data.EscapeJsonText(Encoding.UTF8.GetString(bytes, from, to).Trim());
-                    builder.Append($"\"{s}\"");
-                }
-                else {
+                if (isBinary) {
+                    byte[] bytes = result[i].Data.ToBytes();
+
                     builder.Append("\"0x");
-                    for (int j = from; j < to; j++) {
-                        if (bytes[j] == 0) break;
+                    for (int j = 0; j < bytes.Length; j++) {
                         builder.Append($"{bytes[j]:X2}");
                     }
                     builder.Append('\"');
+                }
+                else {
+                    builder.Append($"\"{Data.EscapeJsonText(result[i].Data.ToString())}\"");
                 }
             }
             else {
@@ -378,6 +367,7 @@ internal static partial class Polling {
         }
 
         builder.Append(']');
+
         return Encoding.UTF8.GetBytes(builder.ToString());
     }
 
