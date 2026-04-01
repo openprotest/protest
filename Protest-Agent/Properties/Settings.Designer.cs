bbc321f0ecf0ab3a32e9d3923ab1758e51b26428
Andreas Venizelou <Bl4ckDn4@msn.com>
2026-04-01 22:25:01 +0300

    Agent 'readonly'

--- Protest-Agent/Properties/Settings.Designer.cs
+++ Protest-Agent/Properties/Settings.Designer.cs
@@ -15,7 +15,7 @@ namespace ProtestAgent.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        private static readonly Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
         public static Settings Default {
             get {
