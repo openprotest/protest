e07b139f5211b1733b8c63029252b4ad806d218e
venizelou andreas <openprotest@protonmail.me>
2026-04-11 11:37:24 +0300

    WebSocket read generalization

--- Protest/Http/WebSocketHelper.cs
+++ Protest/Http/WebSocketHelper.cs
@@ -16,4 +16,32 @@ internal static class WebSocketHelper {
         if (ws.State != WebSocketState.Open) return;
         await ws.SendAsync(new ArraySegment<byte>(data), WebSocketMessageType.Text, true, CancellationToken.None);
     }
+
+    internal static async Task<string> WsReadText(WebSocket ws, CancellationToken token, int bufferSize = 2048) {
+        byte[] buffer = new byte[bufferSize];
+        WebSocketReceiveResult result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+
+        if (result.MessageType == WebSocketMessageType.Close) {
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, result.CloseStatusDescription, token);
+            return null;
+        }
+
+        if (result.EndOfMessage) return Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+        StringBuilder builder = new StringBuilder();
+        builder.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+        while (true) {
+            result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+
+            if (result.MessageType == WebSocketMessageType.Close) {
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, result.CloseStatusDescription, token);
+                return null;
+            }
+
+            builder.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+            if (result.EndOfMessage) return builder.ToString();
+        }
+    }
 }
