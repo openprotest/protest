3030d0f0c221464c8bba65753da3e066265abfce
andreas venizelou <openprotest@protonmail.me>
2026-08-07 19:02:17 +0300

    Address book, reeded glass effect

--- Protest/Front/addressbookwindow.js
+++ Protest/Front/addressbookwindow.js
@@ -1,12 +1,14 @@
 "use strict";
-
-const hsl = [24,85,50];
-document.querySelector(":root").style.setProperty("--clr-select", `hsl(${hsl[0]+5},${hsl[1]}%,${hsl[2]*.9}%)`);
-document.querySelector(":root").style.setProperty("--clr-accent", `hsl(${hsl[0]},${hsl[1]}%,${hsl[2]}%)`);
-
 function SetBarColor() {
 	const bar = document.getElementsByClassName("address-book-search-bar")[0];
-	bar.style.background = `linear-gradient(0deg, hsla(${hsl[0]+5},72.5%,42.5%,.6) 0%, hsla(${hsl[0]},75%,45%,1) 100%)`;
+	bar.style.background = "none";
+	bar.style.boxShadow = "rgba(0,0,0,.4) 0 2px 1px";
+
+	const blocks = document.getElementsByClassName("address-book-block");
+	for (let i=0; i<blocks.length; i++) {
+		const block = blocks[i];
+		block.style.backgroundColor = `hsla(${24+i},${75-i/2}%,${45-i/2}%,${1-i/16})`;
+	}
 
 	const buttons = document.getElementsByClassName("address-book-button");
 	for (let i=0; i<buttons.length; i++) {
