5e67bbdc6135ca4c0e1fa600b783e44f0ab0ab60
andreas venizelou <openprotest@protonmail.me>
2026-08-28 17:55:13 +0300

    Address book CSS filters, favicon typo

--- Protest/Front/fullwindow.js
+++ Protest/Front/fullwindow.js
@@ -264,7 +264,7 @@ class Window {
 		const favicon_light = document.createElement("link");
 		favicon_light.rel = "icon";
 		favicon_light.href = iconPath;
-		favicon_light.media = "(prefers-color-scheme:light";
+		favicon_light.media = "(prefers-color-scheme:light)";
 
 		const favicon_dark = document.createElement("link");
 		favicon_dark.rel = "icon";
